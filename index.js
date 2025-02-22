f0cac35ec202b00a64ed715814c41af1a2891dfd
Debasmita Adhikari <72942875+debasmita-a@users.noreply.github.com>
2025-02-22 21:56:07 +0530

    weather app

---
+++ index.js
@@ -0,0 +1,25 @@
+// WEATHER APP
+const weatherForm = document.querySelector(".weatherform");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apikey = "6268459f83298a5e7f925e72b474dc7a";
+
+weatherForm.addEventListener("submit", event=>{
+
+});
+
+async function getWeatherData(city) {
+    
+}
+
+async function displayWeatherInfo(data) {
+    
+}
+
+async function getWeatherEmoji(weatherId) {
+    
+}
+
+async function displayError(message) {
+    
+}
